13aa16a56ca0f41b9d0d467212427468eb3954be
crusejai <31932042+crusejai@users.noreply.github.com>
2017-11-09 20:33:34 -0500

    Add files via upload

---
+++ website/measure.php
@@ -0,0 +1,20 @@
+<?php session_start() ?>
+
+
+
+<html>
+  <head>
+      <title>Process Two:Measure</title>
+
+
+  </head>
+
+  <body>
+<?php include("measureElements.php"); ?>
+    <?php include("processmeasurement.php"); ?>
+<?php  header("profile.html"); ?>
+
+  </body>
+
+
+  </html>
---
+++ website/reg.php
@@ -0,0 +1,20 @@
+<?php session_start() ?>
+
+
+
+<html>
+  <head>
+      <title>Process One: Registeration</title>
+
+
+  </head>
+
+  <body>
+    <?php include("elementsConvert.php"); ?>
+    <?php include("processregistration.php"); ?>
+
+
+  </body>
+
+
+  </html>
